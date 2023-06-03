50f36af87d437a18126717a7dec5eddee2cce97b
caioba <chiqueto.caio@gmail.com>
2023-06-02 22:19:50 -0300

    Atualização

--- bdconfig.js
+++ bdconfig.js
@@ -3,18 +3,18 @@ module.exports = {
   // NODE_MYSQL_SERVIDOR e faça ela valer o nome ou o
   // endereço IP da máquina que executa o MySQL (se for
   // uma instalação local, faça ela valer LOCALHOST)
-  host: '',
+  host: 'db4free.net',
 
   // crie uma variável de ambiente no Windows chamada
   // NODE_MYSQL_USUARIO e faça ela valer o seu login
-  user: ``,
+  user: 'caioba012',
 
   // crie uma variável de ambiente no Windows chamada
   // NODE_MYSQL_SENHA e faça ela valer sua senha
-  password: ``,
+  password: 'bwe97383149',
 
   // crie uma variável de ambiente no Windows chamada
   // NODE_MYSQL_BD e faça ela valer nome do BD que
   // pretende acessar
-  database: ``
+  database: 'caioba012'
 };
